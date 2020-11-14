ce32259cb3e428ae162c6b1d4cdf00802159997a
Sergei Biriukov <svbiriukov@gmail.com>
2020-11-14 17:42:06 +1100

    Done Wood 1

--- 2020_2_fall_challenge/bot.py
+++ 2020_2_fall_challenge/bot.py
@@ -1,57 +1,176 @@
 import sys
 import math
+import time
+import random
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
+debug = True
 def dp(s):
-    print(s, file=sys.stderr, flush=True)
+  if debug: print(s, file=sys.stderr, flush=True)
 
-# game loop
-while True:
+class Potion:
+  def __init__(self):
+    self.dist = 10**5
+    self.id = 0
+    self.price = 0
+
+class Spell:
+  def __init__(self):
+    self.delta = [0,0,0,0]
+    self.id = 0
+    self.is_ready = False
+
+  def copy(self):
+    t = Spell()
+    t.delta = self.delta[:]
+    t.id = self.id
+    t.is_ready = self.is_ready
+    return t
+
+class State:
+  def __init__(self):
+    self.inv = []
+    self.spells = []
+    self.turns = []
+
+  def copy(self):
+    t = State()
+    t.spells = []
+    for s in self.spells:
+      t.spells.append(s.copy())
+    t.inv = self.inv[:]
+    t.turns = self.turns[:]
+    return t
+
+def is_enough_ingredients(state, potion):
+  for i in range(4):
+    if state.inv[i]+potion.delta[i] < 0: return False
+  return True
+
+def try_cast(state,spell_id):
+  new_state = state.copy()
+  if new_state.spells[spell_id].is_ready:
+    for i in range(4):
+      new_state.inv[i] += new_state.spells[spell_id].delta[i]
+    if min(new_state.inv) < 0:
+      return state.copy(), False
+    if sum(new_state.inv) > 10:
+      return state.copy(), False
+    new_state.spells[spell_id].is_ready = False
+    return new_state, True
+  return state.copy(), False
+
+def rest(state):
+  new_state = state.copy()
+  for s in new_state.spells:
+    s.is_ready = True
+  return new_state
+
+def get_states(state):
+  new_states = []
+  for i in range(len(state.spells)):
+    if state.spells[i].is_ready:
+      new_state, is_possible = try_cast(state, i)
+      if is_possible:
+        new_state.turns.append('CAST '+str(new_state.spells[i].id))
+        new_states.append(new_state)
+    else:
+      new_state = rest(state)
+      new_state.turns.append('REST')
+      new_states.append(new_state)
+  return new_states
+
+def find_solution(state, potion):
+  all_states = [state.copy()]
+
+  
+  if (is_enough_ingredients(all_states[0], potion)):
+    all_states[0].turns.append('BREW '+str(potion.id))
+    # print(i, all_states[0].inv, potion.delta, all_states[0].turns)
+    # dp('found solution in '+str(i)+' steps')
+    return all_states[0]
+
+  max_int = 200
+  for i in range(max_int):
+    # print('Q',len(all_states),all_states[0].turns)
+    if len(all_states) == 0:
+      return state
+    new_states = get_states(all_states[0])
+    for s in new_states:
+      all_states.append(s)
+      if (is_enough_ingredients(s, potion)):
+        s.turns.append('BREW '+str(potion.id))
+        # print(i, s.inv, potion.delta, s.turns)
+        dp('found solution in '+str(i)+' steps')
+        return s
+    del all_states[0]
+  # dp('found solution in '+str(i)+' steps')
+  return state
+
+def main():
+  while True:
+    state = State()
     potions = []
+
     action_count = int(input())
     for i in range(action_count):
-        # action_id: the unique ID of this spell or recipe
-        # action_type: in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
-        # delta_0: tier-0 ingredient change
-        # delta_1: tier-1 ingredient change
-        # delta_2: tier-2 ingredient change
-        # delta_3: tier-3 ingredient change
-        # price: the price in rupees if this is a potion
-        # tome_index: in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax
-        # tax_count: in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
-        # castable: in the first league: always 0; later: 1 if this is a castable player spell
-        # repeatable: for the first two leagues: always 0; later: 1 if this is a repeatable player spell
-        action_id, action_type, delta_0, delta_1, delta_2, delta_3, price, tome_index, tax_count, castable, repeatable = input().split()
-        action_id = int(action_id)
-        delta_0 = int(delta_0)
-        delta_1 = int(delta_1)
-        delta_2 = int(delta_2)
-        delta_3 = int(delta_3)
-        price = int(price)
-        tome_index = int(tome_index)
-        tax_count = int(tax_count)
-        castable = castable != "0"
-        repeatable = repeatable != "0"
-        potions.append([delta_0, delta_1, delta_2, delta_3, price, action_id, action_type, tome_index, tax_count, castable, repeatable])
+      action_id, action_type, delta_0, delta_1, delta_2, delta_3, price, tome_index, tax_count, castable, repeatable = input().split()
+      action_id = int(action_id)
+      delta_0 = int(delta_0)
+      delta_1 = int(delta_1)
+      delta_2 = int(delta_2)
+      delta_3 = int(delta_3)
+      price = int(price)
+      tome_index = int(tome_index)
+      tax_count = int(tax_count)
+      castable = castable != "0"
+      repeatable = repeatable != "0"
+      if action_type == 'BREW':
+        p = Potion()
+        p.delta = [delta_0, delta_1, delta_2, delta_3]
+        p.price = price
+        p.id = action_id
+        potions.append(p)
+      if action_type == 'CAST':
+        s = Spell()
+        s.delta = [delta_0, delta_1, delta_2, delta_3]
+        s.id = action_id
+        s.is_ready = castable
+        state.spells.append(s)
+      # dp((action_id, action_type, delta_0, delta_1, delta_2, delta_3, price, tome_index, tax_count, castable, repeatable))
 
-    players = []
     for i in range(2):
-        inv_0, inv_1, inv_2, inv_3, score = [int(j) for j in input().split()]
-        players.append([inv_0, inv_1, inv_2, inv_3, score])
+      inv_0, inv_1, inv_2, inv_3, score = [int(j) for j in input().split()]
+      if i == 0:
+        state.inv = [inv_0, inv_1, inv_2, inv_3]
 
-    potion_id = -1
-    max_price = -1
+    turn = 'WAIT'
+    d1 = 10000
+    c1 = 1
     for p in potions:
-        is_possible = True
-        for i in range(4):
-            if ((players[0][i] - p[i]) < 0):
-                is_possible = False
-        if (is_possible):
-            if p[4] > max_price:
-                potion_id = p[5]
-                max_price = p[4]
+      solved = find_solution(state, p)
+      if (len(solved.turns) != 0):
+        d2 = len(solved.turns)
+        c2 = p.price
+
+        dd = float(d1)/d2
+        dc = float(c1)/c2
+        if (dc < dd):
+          turn = solved.turns[0]
+          d1 = d2
+          c1 = c2
+      dp((p.id,p.price,len(solved.turns), solved.turns))
+    if (turn == 'WAIT'):
+      for i in range(4):
+        new_state, is_ok = try_cast(state,i)
+        if is_ok:
+          turn = 'CAST '+str(state.spells[i].id)
+          break
+      if turn == 'WAIT':
+        turn = 'REST'
 
     # in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-    print("BREW "+str(potion_id))
+    print(turn)
+
+if (__name__ == "__main__"):
+  random.seed(19081991)
+  main()
---
+++ 2020_2_fall_challenge/test_bot.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+
+import bot
+import time
+
+def test_is_enough_ingredients():
+  s1 = bot.State()
+  s1.inv = [0,0,0,0]
+  p1 = bot.Potion()
+  p1.delta = [-1,0,0,0]
+  r1 = bot.is_enough_ingredients(s1,p1)
+  if r1 == True:
+    print('Failed')
+
+  s2 = bot.State()
+  s2.inv = [1,0,0,0]
+  p2 = bot.Potion()
+  p2.delta = [-1,0,0,0]
+  r2 = bot.is_enough_ingredients(s2,p2)
+  if r2 == False:
+    print('Failed')
+
+def test_try_cast():
+  s = bot.State()
+  s.inv = [0,0,0,0]
+  s.spells = []
+  s.spells.append(bot.Spell())
+  s.spells[0].delta = [2,0,0,0]
+
+  s.spells[0].is_ready = False
+  new_state, is_casted = bot.try_cast(s,0)
+  if is_casted == True:
+    print('Failed: test_try_cast: not ready spell is casted')
+
+  s.spells[0].is_ready = True
+  new_state, is_casted = bot.try_cast(s,0)
+  if is_casted == False:
+    print('Failed: test_try_cast: ready spell is not casted')
+  if new_state.spells[0].is_ready == True:
+    print('Failed: test_try_cast: new spell is ready after cast')
+  if s.spells[0].is_ready == False:
+    print('Failed: test_try_cast: old spell is not ready after cast')
+  if new_state.inv != [2,0,0,0]:
+    print('Failed: test_try_cast: new inventory is wrong after cast')
+  if s.inv != [0,0,0,0]:
+    print('Failed: test_try_cast: old inventory is wrong after cast')
+
+  s.spells[0].delta = [-1,1,0,0]
+  new_state, is_casted = bot.try_cast(s,0)
+  if is_casted == True:
+    print('Failed: test_try_cast: impossible spell is casted')
+  if new_state.inv != [0,0,0,0]:
+    print('Failed: test_try_cast: new inventory is wrong after not cast')
+  if s.inv != [0,0,0,0]:
+    print('Failed: test_try_cast: old inventory is wrong after not cast')
+  if new_state.spells[0].is_ready == False:
+    print('Failed: test_try_cast: new spell is not ready after not cast')
+  if s.spells[0].is_ready == False:
+    print('Failed: test_try_cast: old spell is not ready after not cast')
+
+  s.inv = [3,3,3,0]
+  s.spells[0].delta = [2,0,0,0]
+  new_state, is_casted = bot.try_cast(s,0)
+  if is_casted == True:
+    print('Failed: test_try_cast: impossible spell is casted')
+  if new_state.inv != [3,3,3,0]:
+    print('Failed: test_try_cast: new inventory is wrong after not cast')
+  if s.inv != [3,3,3,0]:
+    print('Failed: test_try_cast: old inventory is wrong after not cast')
+  if new_state.spells[0].is_ready == False:
+    print('Failed: test_try_cast: new spell is not ready after not cast')
+  if s.spells[0].is_ready == False:
+    print('Failed: test_try_cast: old spell is not ready after not cast')
+
+def test_rest():
+  s = bot.State()
+  s.inv = [0,0,0,0]
+  s.spells = []
+  s.spells.append(bot.Spell())
+  s.spells.append(bot.Spell())
+  s.spells[0].delta = [2,0,0,0]
+  s.spells[1].delta = [0,2,0,0]
+  s.spells[0].is_ready = True
+  s.spells[1].is_ready = True
+  new_state = bot.rest(s)
+  if new_state.spells[0].is_ready == False:
+    print('Failed: test_try_cast: not ready spell after rest')
+  if new_state.spells[1].is_ready == False:
+    print('Failed: test_try_cast: not ready spell after rest')
+
+  s.spells[0].is_ready = False
+  s.spells[1].is_ready = True
+  new_state = bot.rest(s)
+  if new_state.spells[0].is_ready == False:
+    print('Failed: test_try_cast: not ready spell after rest')
+  if new_state.spells[1].is_ready == False:
+    print('Failed: test_try_cast: not ready spell after rest')
+
+def test_find_solution():
+  s = bot.State()
+  s.inv = [2,0,0,0]
+  p = bot.Potion()
+  p.delta = [-1,0,0,0]
+  solved = bot.find_solution(s, p)
+  if solved.turns != ['BREW 0']:
+    print("Failed: test_find_solution: v1: should be possible")
+  if s.turns != []:
+    print("Failed: test_find_solution: v1: should not chabge initial state")
+
+  s.inv = [1,0,0,0]
+  p.delta = [-2,0,0,0]
+  solved = bot.find_solution(s, p)
+  if solved.turns != []:
+    print("Failed: test_find_solution: should be impossible")
+
+  s.spells.append(bot.Spell())
+  s.spells[0].delta = [2,0,0,0]
+  s.spells[0].is_ready = True
+  solved = bot.find_solution(s, p)
+  if solved.turns != ['CAST 0', 'BREW 0']:
+    print("Failed: test_find_solution: v3: should be possible")
+
+  s.spells[0].is_ready = False
+  solved = bot.find_solution(s, p)
+  if solved.turns != ['REST', 'CAST 0', 'BREW 0']:
+    print("Failed: test_find_solution: v4: wrong turns returned")
+
+  p.delta = [-1,-1,0,0]
+  s.spells.append(bot.Spell())
+  s.spells[1].delta = [-1,1,0,0]
+  s.spells[1].id = 1
+  solved = bot.find_solution(s, p)
+  if solved.turns != ['REST', 'CAST 0', 'CAST 1', 'BREW 0']:
+    print("Failed: test_find_solution: v5: wrong turns returned")
+
+  s = bot.State()
+  s.inv = [3,0,0,0]
+
+  s.spells.append(bot.Spell())
+  s.spells[0].delta = [2,0,0,0]
+  s.spells[0].is_ready = True
+  s.spells[0].id = 0
+
+  s.spells.append(bot.Spell())
+  s.spells[1].delta = [-1,1,0,0]
+  s.spells[1].is_ready = True
+  s.spells[1].id = 1
+
+  s.spells.append(bot.Spell())
+  s.spells[2].delta = [0,-1,1,0]
+  s.spells[2].is_ready = True
+  s.spells[2].id = 2
+
+  s.spells.append(bot.Spell())
+  s.spells[3].delta = [0,0,-1,1]
+  s.spells[3].is_ready = True
+  s.spells[3].id = 3
+
+  p = bot.Potion()
+  p.delta = [0,-4,0,0]
+  print('main test is here ->')
+  solved = bot.find_solution(s, p)
+  print(solved.turns)
+  # s.spells.append(bot.Spell())
+  # s.spells[1].delta = [-1,1,0,0]
+  # s.spells[1].is_ready = True
+
+def test_real_life():
+  t1 = time.time()
+
+  s = bot.State()
+  s.inv = [3,0,0,0]
+
+  s.spells.append(bot.Spell())
+  s.spells[0].delta = [2,0,0,0]
+  s.spells[0].is_ready = True
+  s.spells[0].id = 0
+
+  s.spells.append(bot.Spell())
+  s.spells[1].delta = [-1,1,0,0]
+  s.spells[1].is_ready = True
+  s.spells[1].id = 1
+
+  s.spells.append(bot.Spell())
+  s.spells[2].delta = [0,-1,1,0]
+  s.spells[2].is_ready = True
+  s.spells[2].id = 2
+
+  s.spells.append(bot.Spell())
+  s.spells[3].delta = [0,0,-1,1]
+  s.spells[3].is_ready = True
+  s.spells[3].id = 3
+
+  p = bot.Potion()
+  p.delta = [0,-4,0,0]
+  print('main test is here ->')
+  bot.init_prefabs()
+  solved = bot.find_solution(s, p)
+  print(solved.turns)
+  t2 = time.time()
+  print(t2-t1)
+  # < 0.015
+
+def test_priority():
+  d1 = 10000
+  p1 = 1
+  d2 = 6
+  p2 = 11
+
+  dd = d1/d2
+  dp = p1/p2
+  if (dp < dd):
+    print('choose 2',dd,dp)
+  else:
+    print('choose 1',dd,dp)
+  
+# test_is_enough_ingredients()
+# test_try_cast()
+# test_rest()
+# test_find_solution()
+
+test_real_life()
+# test_priority()
