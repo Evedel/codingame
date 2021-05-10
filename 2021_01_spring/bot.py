f751a3ada79f7bf138afaa5ac1b0d99e1daa15f4
Sergei Biriukov <svbiriukov@gmail.com>
2021-05-10 19:14:50 +1000

    2021.01: make more modular

--- 2021_01_spring/bot.py
+++ 2021_01_spring/bot.py
@@ -59,77 +59,105 @@ class Cell:
     self.neigh_dir = []
     self.index = 0
     self.richness = 0
+    self.is_tree = False
+    self.tree_size = 0
+    self.is_mine = False
+    self.is_dormant = False
 
-indexed_cells = []
+def read_input_setup():
+  indexed_cells = []
 
-number_of_cells = int(input())
-for i in range(number_of_cells):
-  index, richness, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
-  cell = Cell()
-  cell.index = index
-  cell.richness = richness
-  cell.neigh_index = [neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5]
-  indexed_cells.append(cell)
+  number_of_cells = int(input())
+  for i in range(number_of_cells):
+    index, richness, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
+    cell = Cell()
+    cell.index = index
+    cell.richness = richness
+    cell.neigh_index = [neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5]
+    indexed_cells.append(cell)
 
-for ic in range(len(indexed_cells)):
-  cell = indexed_cells[ic]
-  cell.neigh_cells = []
-  for i in range(len(cell.neigh_index)):
-    n = cell.neigh_index[i]
-    if n != -1:
-      cell.neigh_cells.append(indexed_cells[n])
-      cell.neigh_dir.append(i)
+  return indexed_cells
 
-arena = indexed_cells[0]
+def make_links(indexed_cells):
+  for ic in range(len(indexed_cells)):
+    cell = indexed_cells[ic]
+    cell.neigh_cells = []
+    for i in range(len(cell.neigh_index)):
+      n = cell.neigh_index[i]
+      if n != -1:
+        cell.neigh_cells.append(indexed_cells[n])
+        cell.neigh_dir.append(i)
 
-line = ["." for i in range(13)]
-matrix = [line[:] for i in range(7)]
-visited = [False for i in range(37)]
-startindex = [3,6]
-matrix,visited = hex_to_matrix(arena,matrix,visited,startindex)
-print_matrix(matrix)
+  return indexed_cells
 
-# game loop
-while True:
-    day = int(input())  # the game lasts 24 days: 0-23
-    nutrients = int(input())  # the base score you gain from the next COMPLETE action
-    # sun: your sun points
-    # score: your current score
-    sun, score = [int(i) for i in input().split()]
+def print_arena(indexed_cells):
+  arena = indexed_cells[0]
+  line = ["." for i in range(13)]
+  matrix = [line[:] for i in range(7)]
+  visited = [False for i in range(37)]
+  startindex = [3,6]
+  matrix,visited = hex_to_matrix(arena,matrix,visited,startindex)
+  print_matrix(matrix)
+
+def read_input_turn(indexed_cells):
+  day = int(input())  # the game lasts 24 days: 0-23
+  nutrients = int(input())  # the base score you gain from the next COMPLETE action
+  # sun: your sun points
+  # score: your current score
+  sun, score = [int(i) for i in input().split()]
+  inputs = input().split()
+  opp_sun = int(inputs[0])  # opponent's sun points
+  opp_score = int(inputs[1])  # opponent's score
+  opp_is_waiting = inputs[2] != "0"  # whether your opponent is asleep until the next day
+  number_of_trees = int(input())  # the current amount of trees
+  for i in range(number_of_trees):
     inputs = input().split()
-    opp_sun = int(inputs[0])  # opponent's sun points
-    opp_score = int(inputs[1])  # opponent's score
-    opp_is_waiting = inputs[2] != "0"  # whether your opponent is asleep until the next day
-    number_of_trees = int(input())  # the current amount of trees
+    cell_index = int(inputs[0])  # location of this tree
+    size = int(inputs[1])  # size of this tree: 0-3
+    is_mine = inputs[2] != "0"  # 1 if this is your tree
+    is_dormant = inputs[3] != "0"  # 1 if this tree is dormant
+    c = indexed_cells[cell_index]
+    c.is_tree = True
+    c.tree_size = size
+    c.is_mine = is_mine
+    c.is_dormant = is_dormant
+  number_of_possible_actions = int(input())  # all legal actions
+  for i in range(number_of_possible_actions):
+    possible_action = input()  # try printing something from here to start with
+  return sun,indexed_cells
+
+def main():
+  indexed_cells = read_input_setup()
+  indexed_cells = make_links(indexed_cells)
+  print_arena(indexed_cells)
+
+  # game loop
+  while True:
+    sun,indexed_cells = read_input_turn(indexed_cells)
+
     max_index = 0
     max_size = 0
     size_2_trees = 0
     size_3_trees = 0
-    for i in range(number_of_trees):
-        inputs = input().split()
-        cell_index = int(inputs[0])  # location of this tree
-        size = int(inputs[1])  # size of this tree: 0-3
-        is_mine = inputs[2] != "0"  # 1 if this is your tree
-        is_dormant = inputs[3] != "0"  # 1 if this tree is dormant
-        if is_mine:
-            if size == 2:
-                size_2_trees += 1
-            if size == 3:
-                size_3_trees += 1
+    for c in indexed_cells:
+      if c.is_tree and c.is_mine:
+        if c.tree_size == 2:
+          size_2_trees += 1
+        if c.tree_size == 3:
+          size_3_trees += 1
 
-            if size > max_size:
-                max_size = size
-                max_index = cell_index
-    number_of_possible_actions = int(input())  # all legal actions
-    for i in range(number_of_possible_actions):
-        possible_action = input()  # try printing something from here to start with
+        if c.tree_size > max_size:
+          max_size = c.tree_size
+          max_index = c.index
 
-    # GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
     if (max_size == 1) and (sun >= 3 + size_2_trees):
-        print("GROW "+str(max_index))
+      print("GROW "+str(max_index))
     elif (max_size == 2) and (sun >= 7 + size_3_trees):
-        print("GROW "+str(max_index))
+      print("GROW "+str(max_index))
     elif (max_size == 3) and (sun >= 4):
-        print("COMPLETE "+str(max_index))
+      print("COMPLETE "+str(max_index))
     else:
-        print("WAIT")
+      print("WAIT")
+
+if __name__ == "__main__":
+  main()
